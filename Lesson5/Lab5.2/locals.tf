9b93369212a80895e25083c9a9bbf60eedbfe75f
Phuong Cao <phuongcao@opsspark.com>
2023-02-06 22:31:07 +0700

    Lesson 5: Update lab 5.2
    Signed-off-by: Phuong Cao <phuongcao@opsspark.com>

--- Lesson5/Lab5.2/locals.tf
+++ Lesson5/Lab5.2/locals.tf
@@ -5,16 +5,5 @@ locals {
     "Environment" = var.env
   }
 
-  # user_data = <<-EOT
-  #   #!/usr/bin/env bash
-  #   apt update -y
-  #   apt install nginx -y
-  #   systemctl start nginx
-  #   systemctl enable nginx
-  #   echo "<h1> Hello from $(hostname -f)</h1>" > /var/www/html/index.html
-  #   EOF
-  # EOT
-
-
   name_prefix = "${var.project_name}-${var.env}"
 }
