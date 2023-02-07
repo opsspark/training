d615e8464f013160f844cf54655dcad908ca2b6a
Phuong Cao <phuongcao@opsspark.com>
2023-02-07 08:46:47 +0700

    Lesson 5: Update lab 5.2
    Signed-off-by: Phuong Cao <phuongcao@opsspark.com>

--- Lesson5/Lab5.2/ec2.tf
+++ Lesson5/Lab5.2/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "mytestserver" {
   ami           = "ami-0b7e55206a0a22afc"
   instance_type = "t3.micro"
 
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id, aws_security_group.allow_http_https.id]
+  vpc_security_group_ids = [ aws_security_group.allow_http_https.id]
 
   user_data = var.user_data
   
